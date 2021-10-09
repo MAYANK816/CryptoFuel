a415bc006c68faa07ef0f9aed1720d8e16415e9a
MAYANK816 <mayank0218.cse19@chitkara.edu.in>
2021-10-09 23:06:37 +0530

    CryptoFuel

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Coindata from './Coin';
 import './App.css';
-
 function App() {
+
+const [Coins, setCoin] = useState([]);
+const [search,setsearch] = useState('');
+useEffect(() => {
+  axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=1000&page=1&sparkline=false')
+  .then(res=>{
+    setCoin(res.data);
+   // console.log(res.data);
+  }).catch(error=>{
+    console.log(error);
+  })
+},[]);
+
+const handleChange=(e)=>{
+  setsearch(e.target.value);
+};
+const filteredCoins=Coins.filter(coin => coin.name.toLowerCase().includes(search.toLowerCase()));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="coin-app">
+      <div className="coin_search">
+        <h1 className="coint_text">Search...</h1>
+        <form>
+          <input type="text" placeholder='Search' className="coin_input" onChange={handleChange}></input>
+          </form>
+        
+      </div>
+      {
+        filteredCoins.map(coin=>{
+          return(
+              <Coindata key={coin.id}
+              name={coin.name}
+              image={coin.image}
+              symbol={coin.symbol}
+              price={coin.current_price}
+              volume={coin.total_volume}
+              priceChange={coin.price_change_percentage_24h}
+              />
+
+          )
+        })
+      }
     </div>
   );
 }
